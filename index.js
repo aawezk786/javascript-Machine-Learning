605447ed724418c039773a7413f2412f897d70af
aawezk456 <aawezk456@gmail.com>
2020-08-09 13:44:38 +0530

    Final commit

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const network = new brain.NeuralNetwork();
 network.train([
   { input: [1,2], output: [1] }, // Team 2 wins
   { input: [1,3], output: [1] }, // Team 3 wins
-  { input: [2,3], output: [0] }, // Team 2 wins
-  { input: [2,4], output: [1] }, // Team 4 wins
-  { input: [1,2], output: [0] }, // Team 1 wins
-  { input: [1,3], output: [0] }, // Team 1 wins
-  { input: [3,4], output: [0] }, // Team 3 wins
+  { input: [2,3], output: [0] }, 
+  { input: [2,4], output: [1] }, 
+  { input: [1,2], output: [0] }, 
+  { input: [1,3], output: [0] }, 
+  { input: [3,4], output: [0] }, 
 ]);
 
 const output = network.run([1, 4]);
